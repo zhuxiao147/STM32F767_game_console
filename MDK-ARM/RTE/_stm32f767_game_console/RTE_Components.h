5f6327c8b84ff265baece8bc48ef68e78f7e7cdc
zhuxiao147 <1909961237@.com>
2025-12-13 21:52:54 +0800

    create cubemx project

---
+++ MDK-ARM/RTE/_stm32f767_game_console/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32f767_game_console' 
+ * Target:  'stm32f767_game_console' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/stm32f767_game_console/startup_stm32f767xx.d
@@ -0,0 +1,1 @@
+stm32f767_game_console\startup_stm32f767xx.o: startup_stm32f767xx.s
